6f693b70dbf16bbf1835e6cc4ba079ae49e96365
tanyamishra2710 <53753782+tanyamishra2710@users.noreply.github.com>
2019-09-05 15:39:42 +0530

    Add files via upload

--- book.java
+++ book.java
@@ -29,3 +29,23 @@ class book
 	}
 }
 
+class bookdemo
+{
+  	public static void main(String args[])
+   	{
+    		int i=0;
+    		int ch;
+     		Scanner a=new Scanner(System.in);
+     		System.out.print("Enter number of objects: ");
+     		int n=a.nextInt();
+     		book b[]=new book[10];
+     		while(i<n)
+       		{ 
+         		b[i]=new book();
+			b[i].setdata(); 
+         		System.out.println("Book "+(i+1));
+         		System.out.println(b[i]);
+         		i++;
+           	}
+         }
+}
